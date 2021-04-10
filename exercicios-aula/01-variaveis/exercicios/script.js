8ffdf1f56a8f2991fdda84529a953d2e01982291
Kamila Almeida <kamilafez1@gmail.com>
2021-04-10 15:44:59 -0300

    resolução exercícios aula semana 6

--- exercicios-aula/01-variaveis/exercicios/script.js
+++ exercicios-aula/01-variaveis/exercicios/script.js
@@ -9,26 +9,34 @@
  
 /*  1. Declare uma variável const com a mensagem 'Olá Mundo', exiba no console */
 
-
+     const mensagem= "Olá mundo"
+     console.log(mensagem);
 /* 
 
   2. Declare uma variável let chamada anoNascimento sem atribuir valor
-
+    
 */
-
+    let anoNascimento;
+    console.log(anoNascimento);
 
 /* 
   3. Atribuía o ano que você nasceu na variável anoNascimento e exiba no console
 */
-
+    const anoDeNascimento = 1988;
+    console.log(anoDeNascimento)
 /* 
   4. Declare uma variável chamada `gostoDeProgramar`, atribuindo à ela o valor booleano que representa `verdadeiro` e exiba no console.
 */
-
+    let gostoDeProgramar = true;
+    console.log(gostoDeProgramar);
 
 /* 
   5. Exiba no console os tipos das variáveis criadas a cima utilizando o operador typeof
      ex: console.log(typeof variavel)
 */
+   console.log ("tipo da varíavel mensagem:", typeof mensagem)
+   console.log ("tipo da varíavel anoNascimento:",typeof anoNascimento)
+   console.log ("tipo da varíavel anoDeNascimento:",typeof anoDeNascimento)
+   console.log ("tipo da varíavel gostoDeProgramar:",typeof gostoDeProgramar)
 
 
