3c5f51dc3e25f454241853ab64aa2fed56aec1be
linieprado <lineprados@gmail.com>
2021-04-10 15:49:27 -0300

    Resolução exercicios algoritmo, variaveis e operadores

--- exercicios-aula/01-variaveis/exercicios/script.js
+++ exercicios-aula/01-variaveis/exercicios/script.js
@@ -9,21 +9,28 @@
  
 /*  1. Declare uma variável const com a mensagem 'Olá Mundo', exiba no console */
 
+const mensagem = "Olá mundo";
+//console.log (mensagem); 
 
 /* 
 
   2. Declare uma variável let chamada anoNascimento sem atribuir valor
 
 */
-
-
+let anoNascimento ;
+//console.log (anoNascimento);
 /* 
   3. Atribuía o ano que você nasceu na variável anoNascimento e exiba no console
 */
+const anoDeNascimento = "1988";
+//console.log (anoDeNascimento);
+
 
 /* 
   4. Declare uma variável chamada `gostoDeProgramar`, atribuindo à ela o valor booleano que representa `verdadeiro` e exiba no console.
 */
+let gostoDeProgramar = true;
+//console.log (gostoDeProgramar);
 
 
 /* 
@@ -31,4 +38,7 @@
      ex: console.log(typeof variavel)
 */
 
-
+console.log("tipo da variável mensagem:", typeof mensagem)
+console.log("tipo da variável anoNascimento:", typeof anoNascimento)
+console.log("tipo da variável anoDeNascimento:",typeof anoDeNascimento)
+console.log("tipo da variável gostoDeProgramar:",typeof gostoDeProgramar)
