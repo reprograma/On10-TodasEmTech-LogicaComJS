0326c83e5d1142423ba7b8bb75830d468e1b1f21
Samdrade <samdrades2@gmail.com>
2021-04-10 17:09:34 -0300

    commit de java

--- exercicios-aula/04-loops/exercicios/script.js
+++ exercicios-aula/04-loops/exercicios/script.js
@@ -18,6 +18,12 @@
 */
 
 
+// for(let contador = 0; contador <= 100; contador++ ){
+//   console.log(contador);
+// }
+
+
+
 
 /* 
   2. Utilizando a estrutura de repetição for, mostre no console apenas os números pares de 1 ao 50.
@@ -31,6 +37,15 @@
 /* 
   3. Utilizando a estrutura de repetição while, mostre no console apenas os números ímpares de 50 ao 120.
 */
+
+let contador = 50;
+
+while(contador <= 120){
+  if(contador%2!==0){
+    console.log(contador)
+  }
+  contador+=1;
+}
 
 
 
