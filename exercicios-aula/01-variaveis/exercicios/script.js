3c8ba9ad1cb83b934f14ad28fe382900521c1c11
Aline Alves <alves.aline2@yahoo.com.br>
2021-04-10 15:39:36 -0300

    resolução dos exercicios da aula( variaveis, operadores e algoritmo)

--- exercicios-aula/01-variaveis/exercicios/script.js
+++ exercicios-aula/01-variaveis/exercicios/script.js
@@ -9,26 +9,37 @@
  
 /*  1. Declare uma variável const com a mensagem 'Olá Mundo', exiba no console */
 
-
+    const mensagem = "Olá mundo";
+    console.log(mensagem);
 /* 
 
   2. Declare uma variável let chamada anoNascimento sem atribuir valor
-
-*/
+    */   
+   let anoNascimento;
+   console.log(anoNascimento);
 
 
 /* 
   3. Atribuía o ano que você nasceu na variável anoNascimento e exiba no console
 */
 
+  const anoDeNascimento= "1992";
+  console.log(anoDeNascimento);
+
 /* 
   4. Declare uma variável chamada `gostoDeProgramar`, atribuindo à ela o valor booleano que representa `verdadeiro` e exiba no console.
 */
 
+ let gostoDeProgramar= true;
+ console.log(gostoDeProgramar);
 
 /* 
   5. Exiba no console os tipos das variáveis criadas a cima utilizando o operador typeof
      ex: console.log(typeof variavel)
 */
 
+console.log("tipo de mensagem:", typeof(mensagem));
+console.log("tipo de anoNascimento:", typeof(anoNascimento));
+console.log("tipo de anoDeNAscimento:", typeof(anoDeNascimento));
+console.log("tipo de gostoDeProgramar:", typeof(gostoDeProgramar));
 
