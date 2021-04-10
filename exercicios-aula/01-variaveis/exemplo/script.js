1d068be0532ebd2406ac658445828ae03dfd12b3
Joseane-Guedes <joseane_23@hotmail.com>
2021-04-10 15:38:37 -0300

    exercicios resolvidos de variaveis, operadores e algortitmo

--- exercicios-aula/01-variaveis/exemplo/script.js
+++ exercicios-aula/01-variaveis/exemplo/script.js
@@ -1,6 +1,19 @@
 // 1 -  Utilizando let e const: qual usar?
 // Declare variáveis que contenham o seu nome completo, dia da semana, seu cpf, rg e data de nascimento
 
+let nome = "Viviane Miranda";
+let diaDaSemana = "sábado";
+diaDaSemana = "domingo";
+
+
+const cpf = "14416483724";
+const rg = "24769961-4";
+const dataNascimento = "26/08/1992";
+
 
 
 //  2 - Tipos de dados, usando a funçao typeOf() e console.log(). Agora defina qual é o tipo de valor que você declarou em cada variável.
+
+console.log('diaDaSemana:', diaDaSemana);
+console.log('tipo da variável diaDaSemana:', typeof(diaDaSemana))
+console.log("isso é uma concatenação:", diaDaSemana + cpf, dataNascimento)
--- exercicios-aula/02-operadores/exercicios/script.js
+++ exercicios-aula/02-operadores/exercicios/script.js
@@ -25,11 +25,19 @@
 
   Obs: Utilizar o Number() para converter o tipo da variável em número.
 */ 
+/*
+let numero1 = Number (prompt("insira um numero"));
+let numero2 = Number (prompt("insira um numero"));
 
+let soma = numero1 + numero2;
 
+/*alert("a soma dos seus dois numeros é:" + soma);*/
 
+/*alert (`a soma dos seus dois numeros é ${soma}`); */
+
+/*para essa funcao funcionar tem que usar crase*/
 /* 
-  2. Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
+  2. Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valo r total a ser pago.
 
   - Entrada de dados: Ler o valor do jantar
   - Processamento: calcular a taxa do garçom e valor total a ser pago.
@@ -37,18 +45,38 @@
 
   Obs: Utilizar o Number() para converter o tipo para numero dos dados.
 */
+/*let valorJantar = Number(prompt("Insira o valor do jantar"));
+let taxaDeServico = 0.1;
+let valorFinal = valorJantar + (taxaDeServico * valorJantar);
 
+console.log(`O valor do jantar é: R$ ${valorJantar}`, o valor da taxa de serviço é de : ${taxaDeServico} e o valor final é de: ${valorFinal}`) */
+/*
+let valorJantar = Number(prompt("Insira o valor do jantar"));
+let taxaDeServico = 0.1;
+let valorFinal = valorJantar + (taxaDeServico * valorJantar);
 
+console.log(`O valor do jantar é: R$${valorJantar}, o valor da taxa de serviço é de: ${taxaDeServico*100}% e o valor final é de: R$${valorFinal}`); */
 
+/*A PARTIR DA 4 QUESTAO TEMOS UM SO ALGORITMO*/
 // 4. Declare uma nova variável chamada `calculadora`, e adicione uma instrução somando os valores 10 e 5.
 
+let calculadora = 10 + 5;
+/*console.log(calculadora);*/
 
 // 5.  Atribua à variável `calculadora` todo o valor dela, somando 1, usando o operador de soma abreviado.
+/*calculadora +=1;*/
+//calculadora ++;
+
+calculadora+=1;
 
 
 // 6. Atribua à variável `calculadora` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
 
+calculadora *= 3; 
+
+
+
 // 7. Qual é o valor da variável `calculadora` até aqui?
 
-
+console.log(calculadora)
 
