30e3bb823c26183ea204371fcb9cdf518fd5a7e8
Lais Pereira <lais.pereira.santos92@gmail.com>
2021-04-10 15:39:29 -0300

    resolução dos exercícios das aulas de variávies, operadores e algoritmo

--- exercicios-aula/01-variaveis/exemplo/script.js
+++ exercicios-aula/01-variaveis/exemplo/script.js
@@ -1,6 +1,15 @@
 // 1 -  Utilizando let e const: qual usar?
 // Declare variáveis que contenham o seu nome completo, dia da semana, seu cpf, rg e data de nascimento
+let nome = "Laís Pereira";
+let diaDaSemana = "sábado";
+diaDaSemana = "domingo";
 
+const cpf = "07807257421";
+const rg = "8783724"
+const dataDeNascimento = "02/04/1992"
 
 
 //  2 - Tipos de dados, usando a funçao typeOf() e console.log(). Agora defina qual é o tipo de valor que você declarou em cada variável.
+console.log('diaDaSemana:' , diaDaSemana);
+console.log( 'tipo da variável diaDaSemana:' , typeof(diaDaSemana));
+console.log('isto é uma concatenação:' , diaDaSemana + cpf, dataDeNascimento);
