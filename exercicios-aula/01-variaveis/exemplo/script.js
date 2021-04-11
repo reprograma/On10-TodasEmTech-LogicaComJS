c5c887cf49b99c4483128e3f0a80d47de7eda055
Izabelle Souza <izabellepsouzaa@gmail.com>
2021-04-11 18:56:23 -0300

    atualizaçao

--- exercicios-aula/01-variaveis/exemplo/script.js
+++ exercicios-aula/01-variaveis/exemplo/script.js
@@ -1,6 +1,17 @@
 // 1 -  Utilizando let e const: qual usar?
 // Declare variáveis que contenham o seu nome completo, dia da semana, seu cpf, rg e data de nascimento
 
+let nome = "Izabelle Souza";
+let diaDaSemana = "domingo";
 
+
+
+const cpf = "06302342482"
+const rg = "732383-1";
+const dataNascimento = "11/07/1987";
 
 //  2 - Tipos de dados, usando a funçao typeOf() e console.log(). Agora defina qual é o tipo de valor que você declarou em cada variável.
+
+
+console.log (diaDaSemana);
+console.log(typeof(diaDaSemana));
