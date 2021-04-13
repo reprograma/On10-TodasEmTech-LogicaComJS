7cd4de63f2377c3a11b36964e40d6c3173a25d6b
Andreza Santana <andreza.santana16@gmail.com>
2021-04-13 14:59:49 -0300

    exercicio 3 e 4 da aula

--- exercicios-aula/03-condicoes/exercicios/script.js
+++ exercicios-aula/03-condicoes/exercicios/script.js
@@ -13,8 +13,26 @@
 /* 
   1. Elabore um algoritmo que receba uma letra e determine se é uma vogal ou consoante (Faça duas versões deste código, uma utilizando if-else e outra utilizando switch-case);
 */
+let letraEscolhida = prompt('insira uma letra do alfabeto');
 
+switch (letraEscolhida) {
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'u':
+    console.log('Essa letra é uma vogal')
+    break;
 
+  default:
+    console.log('Essa letra é uma consoante')
+}
+
+if (letraEscolhida === "a" || letraEscolhida === "e" || letraEscolhida === "i" || letraEscolhida === "o" || letraEscolhida === "u") {
+  console.log("A letra escolhida é uma vogal")
+} else {
+  console.log("A letra escolhida é uma consoante")
+};
 
 
 /* 
@@ -22,9 +40,35 @@
 
 */
 
+let numeroEscolhido = prompt('insira um numero entre 1-7');
 
+switch (numeroEscolhido) {
+  case "1":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é domingo.`)
+    break;
+  case "2":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é segunda.`)
+    break;
+  case "3":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é terça.`)
+    break;
+  case "4":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é quarta.`)
+    break;
+  case "5":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é quinta.`)
+    break;
+  case "6":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é sexta.`)
+    break;
+  case "7":
+    alert(`O dia correspondente ao número ${numeroEscolhido} é sábado.`)
+    break;
+  default:
+    alert(`O número ${numeroEscolhido} não tem nenhum correspondente na semana.`)
+}
 
-/* 
+/*
   3. Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se ambos os números forem iguais, mostre uma mensagem no console "Os números são iguais";
 */
 
@@ -33,7 +77,7 @@
   4. Crie um algoritmo que receba três notas de um aluno, calcule sua média e mostre as seguintes mensagens de acordo com cada situação:
 
   Se a media for igual ou maior que 7 - Aprovado
-  Se a media for maior e igual a cinco e menor que 7 - Recuperação 
+  Se a media for maior e igual a cinco e menor que 7 - Recuperação
   Se a media for menor que 5 - Reprovado
 
   Alunos aprovados devem ver a mensagem: Parabéns, você foi aprovado, aproveite suas férias!
