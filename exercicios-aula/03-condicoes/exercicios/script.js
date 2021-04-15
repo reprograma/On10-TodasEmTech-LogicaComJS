e9a9348a091bdc9558634a5066d090780f175896
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-04-14 22:15:12 -0300

    Terminando exercícios de aula

--- exercicios-aula/03-condicoes/exercicios/script.js
+++ exercicios-aula/03-condicoes/exercicios/script.js
@@ -71,6 +71,18 @@ switch(numeroEscolhido){
   3. Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se ambos os números forem iguais, mostre uma mensagem no console "Os números são iguais";
 */
 
+let numero1 = Number(prompt("digite um número qualquer"));
+let numero2 = Number(prompt("Digite outro número qualquer"));
+
+if(numero1 > numero2){
+  console.log(`O ${numero1} é maior que o ${numero2}`)
+} else if(numero2 > numero1){
+  console.log('O ' + numero2 + 'é maior que o ' + numero1)
+} else if(numero1===numero2){
+  console.log(`O ${numero1} e o ${numero2} tem valores iguais`)
+} else{
+  console.log(`Os valores de entrada não são valores válidos`)
+};
 
 /*
   4. Crie um algoritmo que receba três notas de um aluno, calcule sua média e mostre as seguintes mensagens de acordo com cada situação:
@@ -83,4 +95,18 @@ switch(numeroEscolhido){
   Alunos de recuperação devem ver a mensagem: Você está de recuperação!
   Alunos reprovados devem ver a mensagem: Que pena, você foi reprovado.
 */
+
+let nota1 = Number(prompt("escreva a primeira nota"));
+let nota2 = Number(prompt("escreva a segunda nota"));
+let nota3 = Number(prompt("escreva a terceira nota"));
+
+let mediaFinal = (nota1 + nota2 + nota3)/3;
+
+if(mediaFinal>=7){
+  alert("Parabéns, você foi aprovado, aproveite suas férias!")
+} else if(mediaFinal>=5 && mediaFinal<7){
+  alert("Você está de recuperação!")
+} else{
+  alert("Que pena, você foi reprovado.")
+};
 
