0ffc0f7777bd454ce8762f5f316a2a23ca9d03d5
DianaMartine <diana_martine@outlook.com>
2021-04-14 22:40:14 -0300

    resolved exercicio uri 01

--- diana-martine-exercicio-casa/exercicios/exercicios-uri/01-exercicio/script.js
+++ diana-martine-exercicio-casa/exercicios/exercicios-uri/01-exercicio/script.js
@@ -1,1 +1,27 @@
 // [Exercício 01](https://www.urionlinejudge.com.br/judge/pt/problems/view/1038) 
+
+conta();
+
+function conta() {
+  let item = parseInt(prompt("Insira aqui o número correspondente ao item"));
+  let quantidade = parseInt(
+    prompt("Insira aqui a quantidade correspondente ao item")
+  );
+  let total = "";
+  
+  if (item == 1) {
+    total = 4 * quantidade;
+  } else if (item == 2) {
+    total = 4.5 * quantidade;
+  } else if (item == 3) {
+    total = 5 * quantidade;
+  } else if (item == 4) {
+    total = 2 * quantidade;
+  } else if (item == 5) {
+    total = 1.5 * quantidade;
+  } else if ((item = 0 || item > 5)) {
+    alert("Erro, número inválido");
+    conta();
+  }
+  document.writeln(`Total R$: ${total.toFixed(2)}`);
+}
