66b6a6008cd224c9169689482df13f0c591c70a4
DianaMartine <diana_martine@outlook.com>
2021-04-14 22:35:41 -0300

    create directories with my name

---
+++ diana-martine-exercicios-aula/01-variaveis/exemplo/script.js
@@ -0,0 +1,6 @@
+// 1 -  Utilizando let e const: qual usar?
+// Declare variáveis que contenham o seu nome completo, dia da semana, seu cpf, rg e data de nascimento
+
+
+
+//  2 - Tipos de dados, usando a funçao typeOf() e console.log(). Agora defina qual é o tipo de valor que você declarou em cada variável.
---
+++ diana-martine-exercicios-aula/04-loops/exercicios/script.js
@@ -0,0 +1,49 @@
+/**
+    Documentação para consulta
+
+    for
+    https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/for
+
+    do while
+    https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/do...while
+
+    while
+    https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while
+
+ */
+
+/* 
+  1. Utilizando a estrutura de repetição for, mostre no console todos números 0 ao 100,
+  inclusive esses 2
+*/
+
+
+
+/* 
+  2. Utilizando a estrutura de repetição for, mostre no console apenas os números pares de 1 ao 50.
+  - Números pares são todos os números que divididos por 2 tem resto 0
+  - Qual operador que retorna o resto da divisão? Vamos usar ele!
+*/
+
+
+
+
+/* 
+  3. Utilizando a estrutura de repetição while, mostre no console apenas os números ímpares de 50 ao 120.
+*/
+
+
+
+/* 
+  3. Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
+repetição "while"
+*/
+ 
+
+
+
+
+/* 
+  4. Seu Miguel da vendinha da esquina, não sabe multiplicar. Faça para ele um programa que, dado um número, ele printe (console.log) a tabuada do mesmo de 1 a 10.
+
+*/
