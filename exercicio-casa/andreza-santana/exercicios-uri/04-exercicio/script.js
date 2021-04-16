379a1a0c2b3060548e775b10da807c88f215e303
Andreza Santana <andreza.santana16@gmail.com>
2021-04-16 18:18:31 -0300

    exercicios uri

--- exercicio-casa/andreza-santana/exercicios-uri/04-exercicio/script.js
+++ exercicio-casa/andreza-santana/exercicios-uri/04-exercicio/script.js
@@ -1,1 +1,16 @@
 //[Exercício 04](https://www.urionlinejudge.com.br/judge/pt/problems/view/1009)
+
+while (true) {
+    let nomeVendedor = prompt('insira o nome do vendedor:');
+    let valorVendas = Number(prompt('valor total de vendas:'));
+    const salariofixo = 750.00;
+
+    if (isNaN(valorVendas)) {
+        alert('Por favor inserir um valor de vendas no formato "100.00"');
+    } else {
+        let comissao = valorVendas * 0.15;
+        let calculoSalario = salariofixo + comissao;
+        alert(`Olá ${nomeVendedor} você irá receber: ${calculoSalario.toFixed(2)}!`);
+        break
+    }
+}
