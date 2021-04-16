33f45268686ca16bbc725cd107a90b35a5e64275
Dandara Sousa <dandaralsousa@gmail.com>
2021-04-16 19:43:56 -0300

    Exercicios de casa

---
+++ exercicio-casa/dandara-sousa/funcoes/01-exercicio/script.js
@@ -0,0 +1,7 @@
+//1. Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+let n = Number(prompt("insira um numero"));
+if (n % 2 === 0){
+    console.log("par");
+} else{
+    console.log("impar");
+}
--- exercicios-aula/01-variaveis/exemplo/script.js
+++ exercicios-aula/01-variaveis/exemplo/script.js
@@ -1,6 +1,15 @@
 // 1 -  Utilizando let e const: qual usar?
 // Declare variáveis que contenham o seu nome completo, dia da semana, seu cpf, rg e data de nascimento
 
+let nome = "Dandara Sousa"
+let diaDaSemana = "sábado"
+diaDaSemana = "domingo"
 
+const cpf = "053703113-82"
+const rg = "20050014154"
+const dataNascimento = "23/03/1993"
 
 //  2 - Tipos de dados, usando a funçao typeOf() e console.log(). Agora defina qual é o tipo de valor que você declarou em cada variável.
+
+console.log(diaDaSemana);
+console.log(typeof(diaDaSemana));
