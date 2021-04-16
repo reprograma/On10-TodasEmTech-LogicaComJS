f56ce5ea54364d291d9c806ee04e9186b708fbc7
Elciele Correa <elcielecorrea@gmail.com>
2021-04-16 20:51:07 -0300

    exercicio casa respondido

---
+++ exercicio-casa/elciele-correa/funcoes/03-exercicio/script.js
@@ -0,0 +1,7 @@
+//3. Crie uma função que calcula 5% de desconto retornando o valor do desconto.
+
+let valorInicial = Number(prompt("Insira um valor"));
+let desconto = 0.05;
+let valorDesconto = desconto*valorInicial;
+
+console.log(`O desconto é de: R$${valorDesconto}`);
