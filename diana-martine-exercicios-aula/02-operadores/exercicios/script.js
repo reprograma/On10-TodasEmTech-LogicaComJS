f839472e410f39858013c055c1444e7ca63d1109
DianaMartine <diana_martine@outlook.com>
2021-04-16 20:30:39 -0300

    resolved exercicio 02-operadores

--- diana-martine-exercicios-aula/02-operadores/exercicios/script.js
+++ diana-martine-exercicios-aula/02-operadores/exercicios/script.js
@@ -25,9 +25,11 @@
 
   Obs: Utilizar o Number() para converter o tipo da variável em número.
 */ 
+/* let number1 = Number(prompt("Insira um número"));
+let number2 = Number(prompt("Insira outro número"));
+let sum = number1 + number2;
 
-
-
+console.log(sum); */
 /* 
   2. Crie um algoritmo que leia o valor de um jantar, calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
 
@@ -37,18 +39,23 @@
 
   Obs: Utilizar o Number() para converter o tipo para numero dos dados.
 */
+/* let count = Number(prompt("Insira o valor da conta"));
+let service = 0.1;
+let finalValue =  count + (service*count);
 
+console.log(finalValue); */
 
 
 // 4. Declare uma nova variável chamada `calculadora`, e adicione uma instrução somando os valores 10 e 5.
-
+//let calculadora = 10 + 5;
+//console.log(calculadora);
 
 // 5.  Atribua à variável `calculadora` todo o valor dela, somando 1, usando o operador de soma abreviado.
-
+//calculadora ++;
 
 // 6. Atribua à variável `calculadora` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
-
+//calculadora = calculadora *= 3;
 // 7. Qual é o valor da variável `calculadora` até aqui?
-
+//console.log(calculadora);
 
 
