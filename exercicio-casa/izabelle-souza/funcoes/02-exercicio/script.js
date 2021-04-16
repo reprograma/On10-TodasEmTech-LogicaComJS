6f822f40818e7abc4039a87155f972a50a27a1b4
Izabelle Souza <izabellepsouzaa@gmail.com>
2021-04-16 20:20:39 -0300

    atualizando o exercício

--- exercicio-casa/izabelle-souza/funcoes/02-exercicio/script.js
+++ exercicio-casa/izabelle-souza/funcoes/02-exercicio/script.js
@@ -1,1 +1,7 @@
 //2. Crie uma função que recebe 2 parâmetros e retorna a multiplicação deles.
+
+function multiplicador(num1, num2){
+    return "A multiplicação entre os números é" + num1 * num2;
+}
+
+console.log(multiplicador(2,3));
