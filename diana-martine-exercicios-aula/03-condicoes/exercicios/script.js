23b95f4d74ab7206ee942cb85931fd162e7dbb38
DianaMartine <diana_martine@outlook.com>
2021-04-16 21:43:46 -0300

    resolved exercicio 03-condicoes

--- diana-martine-exercicios-aula/03-condicoes/exercicios/script.js
+++ diana-martine-exercicios-aula/03-condicoes/exercicios/script.js
@@ -13,7 +13,25 @@
 /* 
   1. Elabore um algoritmo que receba uma letra e determine se é uma vogal ou consoante (Faça duas versões deste código, uma utilizando if-else e outra utilizando switch-case);
 */
+/* let letra = prompt("Insira uma letra qualquer do alfabeto").toLowerCase();
 
+switch (letra) {
+  case "a":
+  case "e":
+  case "i":
+  case "o":
+  case "u":
+    console.log('Essa letra é uma vogal');
+    break;
+    default:
+      console.log("Essa letra é uma consoante");
+}
+
+if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+  console.log("Essa letra é uma vogal");
+} else {
+  console.log("Essa letra é uma consoante");
+} */
 
 
 
@@ -21,13 +39,43 @@
   2. Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente;
 
 */
+/* let number = prompt("Insira um número de 1 a 7");
 
+switch (number) {
+  case "1":
+    alert("O dia da semana correspondente ao valor inserido é: domingo");
+    break;
+    case "2":
+    alert("O dia da semana correspondente ao valor inserido é: segunda");
+    break;case "3":
+    alert("O dia da semana correspondente ao valor inserido é: terça");
+    break;case "4":
+    alert("O dia da semana correspondente ao valor inserido é: quarta");
+    break;case "5":
+    alert("O dia da semana correspondente ao valor inserido é: quinta");
+    break;case "6":
+    alert("O dia da semana correspondente ao valor inserido é: sexta");
+    break;case "7":
+    alert("O dia da semana correspondente ao valor inserido é: sábado");
+    break;
+    default:
+      alert("Número inválido")
+} */
 
 
 /* 
   3. Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, se ambos os números forem iguais, mostre uma mensagem no console "Os números são iguais";
 */
+/* let number1 = Number(prompt("Insira um número"));
+let number2 = Number(prompt("Insira outro número"));
 
+if (number1 > number2) {
+  console.log(`O maior valor é ${number1}`);
+} else if (number2 > number1) {
+  console.log(`O maior valor é ${number2}`);
+} else if (number1 == number2) {
+  console.log("Os valores são iguais");
+} */
 
 /*
   4. Crie um algoritmo que receba três notas de um aluno, calcule sua média e mostre as seguintes mensagens de acordo com cada situação:
@@ -41,3 +89,15 @@
   Alunos reprovados devem ver a mensagem: Que pena, você foi reprovado.
 */
 
+/* let nota1 = Number(prompt("Insira o primeira nota"));
+let nota2 = Number(prompt("Insira o segunda nota"));
+let nota3 = Number(prompt("Insira o terceira nota"));
+let media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+  alert("Parabéns, você foi aprovado, aproveite suas férias!")
+} else if (media >= 5 && media < 7) {
+  alert("Você está de recuperação!");
+} else if (media < 5) {
+  alert("Que pena, você foi reprovado.")
+} */
