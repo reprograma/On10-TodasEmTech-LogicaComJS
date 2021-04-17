bd08efa05179dcbedd8b956dc7b234380d17f3af
Gabrielle Oliveira <biela.a09@gmail.com>
2021-04-16 22:01:00 -0300

    Exercício Função #01.

--- exercicio-casa/exercicios/funcoes/01-exercicio/script.js
+++ exercicio-casa/exercicios/funcoes/01-exercicio/script.js
@@ -1,1 +1,10 @@
 //1. Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+
+function name(params) {
+    if (params % 2 == 0) {
+        return 'Par';
+    } else {
+        return 'impar';
+    }
+}
