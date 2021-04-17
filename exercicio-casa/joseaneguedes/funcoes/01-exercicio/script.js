134f3f0bb482a15a12c3a3adf9c72cf581a2cbb9
Joseane-Guedes <joseane_23@hotmail.com>
2021-04-17 00:16:52 -0300

    Atualizaçao final do exercici principal

--- exercicio-casa/joseaneguedes/funcoes/01-exercicio/script.js
+++ exercicio-casa/joseaneguedes/funcoes/01-exercicio/script.js
@@ -1,1 +1,17 @@
 //1. Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+/*
+if (condicao) {
+  codigo para executar caso a condição seja verdadeira
+} else {
+  senão, executar este código
+} */
+
+function verification(number) {
+  if (number % 2 == 0) {
+    return 'Par'
+  } else {
+    return 'ìmpar'
+  }
+
+}
+alert(verification(prompt("Digite um numero qualquer:")))
--- exercicio-casa/joseaneguedes/funcoes/04-exercicio/script.js
+++ exercicio-casa/joseaneguedes/funcoes/04-exercicio/script.js
@@ -1,2 +1,17 @@
 //4. Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
 
+
+const ano = 2009
+
+function eMaior(nascimento) {
+    const hoje = new Date() .getFullYear()
+    if(hoje - nascimento > 18) return true
+    return false
+}
+
+if (eMaior(ano)) {
+    console.log('É maior de idade')
+} else (eMaior(ano)){
+    console.log('É maior de idade')
+}
+
