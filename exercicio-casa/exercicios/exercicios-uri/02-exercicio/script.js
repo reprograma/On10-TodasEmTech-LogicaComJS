4bada6c20fe60f6fd86079167cdac042320e3a2a
Mariana Herreros <maribranding@gmail.com>
2021-04-16 21:08:32 -0300

    Resolução dos Exercícios

--- exercicio-casa/exercicios/exercicios-uri/02-exercicio/script.js
+++ exercicio-casa/exercicios/exercicios-uri/02-exercicio/script.js
@@ -1,10 +1,9 @@
 //- [Exercício 02](https://www.urionlinejudge.com.br/judge/pt/problems/view/1134)
+
 let entrada = 1
 let alcool = 2
 let diesel = 3
 let gasolina = 4
-
-
 
 while(entrada == 1 || entrada ==2 || entrada ==3 || entrada == 4){
 
@@ -35,16 +34,3 @@ while((entrada =! 1) && (entrada =!2) && (entrada =! 3) && (entrada =! 4)){
 }
 
 }
-
-console.log (alcool)
-console.log(gasolina)
-console.log(gasolina)
-
-do {
-    let entrada= prompt("insira texto aqui");
-    while((entrada =! 1) && (entrada =!2) && (entrada =! 3) && (entrada =! 4)){
-    let entrada= prompt("insira um novo código"); 
-    if (entrada == 1 || entrada ==2 || entrada ==3 || entrada == 4){
-        break
-    }
-    }
