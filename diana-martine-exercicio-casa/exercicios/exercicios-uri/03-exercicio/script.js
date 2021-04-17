107fcde88369bd48c1587b575933d6e87c05bc03
DianaMartine <diana_martine@outlook.com>
2021-04-16 22:59:51 -0300

    resolved all

--- diana-martine-exercicio-casa/exercicios/exercicios-uri/03-exercicio/script.js
+++ diana-martine-exercicio-casa/exercicios/exercicios-uri/03-exercicio/script.js
@@ -1,1 +1,14 @@
 //[Exercício 03](https://www.urionlinejudge.com.br/judge/pt/problems/view/1114)
+
+/* login();
+
+function login() {
+    let senha = Number(prompt('Insira a senha'));
+
+    if (senha !== 2002) {
+        alert("Erro, senha inválida");
+        login();
+    } else {
+        alert("Acesso permitido");
+    }
+} */
--- diana-martine-exercicio-casa/exercicios/exercicios-uri/04-exercicio/script.js
+++ diana-martine-exercicio-casa/exercicios/exercicios-uri/04-exercicio/script.js
@@ -1,1 +1,13 @@
 //[Exercício 04](https://www.urionlinejudge.com.br/judge/pt/problems/view/1009)
+/* 
+salario();
+
+function salario() {
+  let nome = prompt("Insira aqui seu nome");
+  let salario = Number(prompt("Insira aqui seu sálario fixo"));
+  let vendas = Number(prompt("Insira aqui o valor de vendas efetuadas no mês atual"));
+  let comissao = 0.15;
+  let salariofinal = salario + (vendas*comissao);
+
+  alert(`${nome}\n${salario}\n${vendas}\n\nTotal = R$ ${salariofinal.toFixed(2)}`)
+} */
