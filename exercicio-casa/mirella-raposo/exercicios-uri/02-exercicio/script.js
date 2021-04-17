e4c7884005b0bf72fb512395ab6d41187488bb51
Mirella Raposo <mirellacruzr@gmail.com>
2021-04-16 21:14:57 -0300

    Resolução completa dos exercícios

--- exercicio-casa/mirella-raposo/exercicios-uri/02-exercicio/script.js
+++ exercicio-casa/mirella-raposo/exercicios-uri/02-exercicio/script.js
@@ -1,2 +1,41 @@
 //- [Exercício 02](https://www.urionlinejudge.com.br/judge/pt/problems/view/1134)
 
+let combustivel = Number (prompt("Informe o código correspondente a sua preferência de combustível. 1. Álcool 2. Gasolina 3. Diesel 4. Fim"));
+
+let contAlcool = 0
+
+let contGasolina = 0
+
+let contDiesel = 0
+
+while (combustivel != 4) {
+
+    if (combustivel < 4) {
+
+        if (combustivel == 1) {
+        contAlcool++;
+        }
+
+        if (combustivel == 2) {
+        contGasolina++;
+        }
+
+        if (combustivel == 3) {
+        contDiesel++;
+        }
+
+        alert(`MUITO OBRIGADO! Álcool: ${contAlcool} Gasolina: ${contGasolina} Diesel: ${contDiesel}.`)
+        combustivel = Number (prompt("Informe o código correspondente a sua preferência de combustível. 1. Álcool 2. Gasolina 3. Diesel 4. Fim"));
+
+    }
+
+    if(combustivel > 4) {
+
+        alert("Por favor, insira um código válido.");
+        combustivel = Number (prompt("Informe o código correspondente a sua preferência de combustível. 1. Álcool 2. Gasolina 3. Diesel 4. Fim"));
+
+    }
+
+}
+
+alert("Fim.");
--- exercicio-casa/mirella-raposo/exercicios-uri/03-exercicio/script.js
+++ exercicio-casa/mirella-raposo/exercicios-uri/03-exercicio/script.js
@@ -1,1 +1,12 @@
-//[Exercício 03](https://www.urionlinejudge.com.br/judge/pt/problems/view/1114)
+// https://www.urionlinejudge.com.br/judge/pt/problems/view/1114
+
+let senhaValida = 2002;
+
+let acesso = prompt("Digite a senha correta:");
+
+while (acesso != senhaValida) {
+    alert("Senha incorreta");
+    acesso = prompt("Digite a senha correta:");
+}
+
+alert("Acesso Permitido.");
