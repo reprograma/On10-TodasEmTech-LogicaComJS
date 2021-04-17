51492209830a381b4b12550c56d394b90e4caf84
Rafaela França <rafaelafrancamedeiros@gmail.com>
2021-04-16 23:51:03 -0300

    resolucao exercicos 1 e 2

--- rafaela-franca/exercicios/exercicios-uri/02-exercicio/script.js
+++ rafaela-franca/exercicios/exercicios-uri/02-exercicio/script.js
@@ -1,3 +1,23 @@
 //- [Exercício 02](https://www.urionlinejudge.com.br/judge/pt/problems/view/1134)
 
+let a=0
+let g=0
+let d=0
+
+let codigo = prompt('Digite o código');
+
+while (codigo!="4"){
+    if(codigo==="1"){
+        a=a+1
+        codigo = prompt("Digite o código")
+    }else if(codigo==="2"){
+        g=g+1
+        codigo = prompt('Digite o código')
+    }else if(codigo==="3"){
+        d=d+1
+        codigo = prompt('Digite o código')
+    }else{prompt
+        codigo = 
+    }
+}
 
